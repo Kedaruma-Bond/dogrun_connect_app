a4d4cd9685bbfcfd9fb90edf13b94dd285f1c6ed
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-06-01 01:30:47 +0900

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,20 +68,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'dogrun-connect.herokuapp.com'
+  host = 'dogrun-connect.heroku.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    # port: :ENV['MAILGUN_SMTP_PORT'],
-    # address: :ENV['MAILGUN_SMTP_SERVER'],
-    # user_name: :ENV['MAILGUN_SMTP_LOGIN'],
-    # password: :ENV['MAILGUN_SMTP_PASSWORD'],
-    # domain: :host,
-    # authentication: :plain
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => host,
     :authentication => :plain
   }
 
