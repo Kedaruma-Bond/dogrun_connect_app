7738e1b7c34769568da3cea442e0a886c6528888
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-06-02 01:13:40 +0900

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://dogrun-connect.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :user_name => Rails.application.credentials.contacts[:MAILER_USER_ID],
-    :password => Rails.application.credentials.contacts[:MAILER_PASSWORD],
-    :domain => 'example.com',
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: Rails.application.credentials.contacts[:MAILER_USER_ID],
+    password: Rails.application.credentials.contacts[:MAILER_PASSWORD],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
