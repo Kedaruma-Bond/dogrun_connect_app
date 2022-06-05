7bad7f3f0e974411229002cd4cec81ea2c56553f
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-06-05 12:20:50 +0900

    禁断の所業は意味なかった模様

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # これは絶対にtrueにしないこと
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
