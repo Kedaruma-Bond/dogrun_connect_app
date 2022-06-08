3d1099aed6280ac6d841708c92d32b855039634c
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-06-08 14:01:18 +0900

    redis 確認その5

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,34 +6,34 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def confirm
+    @contact = Contact.new(@contact_params)
+    session[:contact] = @contact.hash
+    return unless @contact.invalid?
+
+    render :new
+  end
+
   def back
     @contact = Contact.new(session[:contact])
     session.delete(:contact)
     render :new, status: :unprocessable_entity
   end
 
-  def confirm
-    @contact = Contact.new(contact_params)
-    session[:contact] = @contact
-    return unless @contact.invalid?
-
-    render :new
-  end
-
   def create
-    @contact = Contact.new(session[:contact])
-    if @contact.save
+    # @contact = Contact.new(session[:contact])
+    if Contact.create(session[:contact])
       ContactMailer.send_mail(@contact).deliver_now
       redirect_to '/', notice: t('.notice')
       session.delete(:cotact)
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entit, error: t('.send_mail_fail')
     end
   end
 
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :email, :message)
+    @contact_params = params.require(:contact).permit(:name, :email, :message)
   end
 end
