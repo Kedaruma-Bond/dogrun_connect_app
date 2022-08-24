351086586b84b838de9d38fb8220f4792d91e439
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-08-24 16:43:26 +0900

    #57 serviceworker読み込まない

---
+++ app/javascript/controllers/service-worker_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect () {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function() {
+        navigator.serviceWorker.register("/service-worker.js", {scope: "./" }).then(function (registration) {
+          console.log("ServiceWorker registration successful with scope: ", registration.scope);
+        }, function (err) {
+          console.log("ServiceWorker registration failed: ", err);
+        });
+      });
+    }
+  }
+}
