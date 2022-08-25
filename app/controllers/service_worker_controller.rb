ea0890740afb455e91c13ade892349e072569354
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-08-26 02:06:05 +0900

    #57 PWA化はとりあえずここまで

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,5 +1,7 @@
 class ServiceWorkerController < ApplicationController
   protect_from_forgery except: :service_worker
+  skip_before_action :require_login
+
   def service_worker; end
 
   def offline; end
