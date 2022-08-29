e90d0161238875f2fe5089fd0e6932016bce6fb0
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-08-29 15:41:20 +0900

    #61 通信中のボタン遷移stimulus controller実装

---
+++ app/javascript/controllers/disable_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { with: String }
+
+  connect() {
+    this.element.dataset['action'] = 'submit->disable#disableForm'
+
+    if (!this.hasWithValue) {
+      this.withValue = t('defaults.processing')
+    }
+  }
+
+  disableForm() {
+    this.submitButtons().forEach(button => {
+      button.disabled = true
+      button.value = this.withValue
+    })
+  }
+
+  submitButtons() {
+    return this.element.querySelectorAll("input[type='submit']")
+  }
+}
