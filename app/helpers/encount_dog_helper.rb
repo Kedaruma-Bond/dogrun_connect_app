d28206307b8c85097ce26c8d8c71c6c78a1622a6
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-10-15 00:36:29 +0900

    #101 バッジ表示機能実装

--- app/helpers/encount_dog_helper.rb
+++ app/helpers/encount_dog_helper.rb
@@ -1,11 +1,15 @@
 module EncountDogHelper
   def new_encount_dog_count_badge
     current_user_new_encount_dogs = EncountDog.where(user_id: current_user.id).where(acknowledge: false)
-    
     @current_user_new_encount_dogs_count = current_user_new_encount_dogs.count
+    return tag.div @current_user_new_encount_dogs_count, class: "w-4 h-4 rounded-full text-center bg-red-500 text-white text-xs ml-3"
   end
 
   def new_encount_dog_badge(encount_dog)
-    
+    if encount_dog.acknowledge == false
+      return tag.div class: "w-2 h-2 rounded-full text-center bg-red-500 text-xs mx-2"
+    else
+      return
+    end
   end
 end
