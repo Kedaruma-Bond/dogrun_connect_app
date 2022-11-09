8c8e5098c747790143ad90a2eafd43096d0beb3c
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-11-09 13:57:35 +0900

    #95 まだダメでした

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,27 +1,27 @@
 class Article < ApplicationRecord
   belongs_to :post
   mount_uploader :image_attach, AttachmentUploader
-  has_one_attached :as_image_attach
+  # has_one_attached :as_image_attach
 
   # validates
   validates :content, presence: true, length: { maximum: 400 }
 
-  after_commit do
-    update_active_storage if previous_changes.keys.include?('image_attach')
-  end
+  # after_commit do
+  #   update_active_storage if previous_changes.keys.include?('image_attach')
+  # end
 
-  def update_active_storage
-    self.as_image_attach.purge if self.as_image_attach.attached?
-    sync_image_attach if self.image_attach.present?
-  end
+  # def update_active_storage
+  #   self.as_image_attach.purge if self.as_image_attach.attached?
+  #   sync_image_attach if self.image_attach.present?
+  # end
 
-  def sync_image_attach
-    image = self.image_attach
-    image.cache_stored_file!
-    file = image.sanitized_file.file
-    content_type = image.content_type
-    self.as_image_attach.attach(io: file, content_type: content_type, filename: self.attributes['image_attach'])
-  end
+  # def sync_image_attach
+  #   image = self.image_attach
+  #   image.cache_stored_file!
+  #   file = image.sanitized_file.file
+  #   content_type = image.content_type
+  #   self.as_image_attach.attach(io: file, content_type: content_type, filename: self.attributes['image_attach'])
+  # end
 end
 
 # == Schema Information
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,7 @@ class Dog < ApplicationRecord
   has_many :entries, dependent: :destroy
   has_many :friend_dogs, dependent: :destroy
   mount_uploader :thumbnail_photo, ThumbnailUploader
-  has_one_attached :as_thumbnail_photo
+  # has_one_attached :as_thumbnail_photo
 
   # validates
   validates :name, presence: true, length: { maximum: 50 }
@@ -23,22 +23,22 @@ class Dog < ApplicationRecord
   scope :dogrun_place_id, -> (id) { joins(:registration_numbers).where(registration_numbers: { dogrun_place_id: id }).includes(:registration_numbers, :user).order(id: :desc)}
   scope :dogrun_place_id_for_friend_dog, -> (id) { joins(:entries, :registration_numbers).where(entries: { exit_at: nil }).where(registration_numbers: { dogrun_place_id: 2 }).includes(:entries, :registration_numbers, :user)}
 
-  after_commit do
-    update_active_storage if previous_changes.keys.include?('thunbnail_photo')
-  end
+  # after_commit do
+  #   update_active_storage if previous_changes.keys.include?('thunbnail_photo')
+  # end
 
-  def update_active_storage
-    self.as_thumbnail_photo.purge if self.as_thumbnail_photo.attached?
-    sync_thumbnail_photo if self.thumbnail_photo.present?
-  end
+  # def update_active_storage
+  #   self.as_thumbnail_photo.purge if self.as_thumbnail_photo.attached?
+  #   sync_thumbnail_photo if self.thumbnail_photo.present?
+  # end
 
-  def sync_thumbnail_photo
-    photo = self.thumbnail_photo
-    photo.cache_stored_file!
-    file = photo.sanitized_file.file
-    content_type = photo.content_type
-    self.as_thumbnail_photo.attach(io: file, content_type: content_type, filename: self.attributes['thumbnail_photo'])
-  end
+  # def sync_thumbnail_photo
+  #   photo = self.thumbnail_photo
+  #   photo.cache_stored_file!
+  #   file = photo.sanitized_file.file
+  #   content_type = photo.content_type
+  #   self.as_thumbnail_photo.attach(io: file, content_type: content_type, filename: self.attributes['thumbnail_photo'])
+  # end
 end
 
 # == Schema Information
