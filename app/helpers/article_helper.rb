a010214ff1486fb7f34ebef1b0d1d6c139c65082
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-11-17 12:28:48 +0900

    #95 typo修正

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -10,7 +10,7 @@ module ArticleHelper
 
   def article_photo_for_preview(article)
     if article.photo.attached?
-      cl_image_tag(article.photo, gravity: :auto, quality_auto: :good, fetch_format: :auto, "data-preview-target": "imagePreview", alt: "preview of article's photo", class: "object-cover mx-auto max-w-max-1/2 max-h-max-1/2")
+      cl_image_tag(article.photo.key, gravity: :auto, quality_auto: :good, fetch_format: :auto, "data-preview-target": "imagePreview", alt: "preview of article's photo", class: "object-cover mx-auto max-w-max-1/2 max-h-max-1/2")
     else
       # 小さい画像ファイルを表示させておき、添付画像と差し替える
       image_tag 'data:image/gif;base64,r0lgodlhaqabaaaaach5baekaaealaaaaaabaaeaaaictaeaow==', "data-preview-target": "imagePreview"
