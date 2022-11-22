a3a6ba10209f574a2b19a1f60e8c5b50c5519ad1
Kedaruma-Bond <brianbondwhippet@gmail.com>
2022-11-22 15:47:42 +0900

    #71 style修正

---
+++ app/javascript/controllers/accordion_controller.js
@@ -0,0 +1,82 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ['summary', 'content']
+
+  connect() {
+    this.resetState()
+  }
+
+  resetState() {
+    this.animation = null
+    this.isClosing = false
+    this.isExpanding = false
+  }
+
+  toggle(event) {
+    event.preventDefault()
+
+    this.element.style.overflow = 'hidden'
+
+    if (this.isClosing || !this.element.open) {
+      this.open()
+    } else if (this.isExpanding || this.element.open) {
+      this.shrink()
+    }
+  }
+
+  open() {
+    this.element.style.height = `${ this.element.offsetHeight }px`
+    this.element.open = true
+
+    window.requestAnimationFrame(() => this.expand())
+  }
+
+  expand() {
+    this.isExpanding = true
+    
+    const startHeight = `${ this.element.offsetHeight }px`
+    const endHeight = `${ this.summaryTarget.offsetHeight + this.contentTarget.offsetHeight }px`
+
+    if (this.animation) {
+      this.animation.cancel()
+    }
+
+    this.animation = this.animate(startHeight, endHeight)
+
+    this.animation.onfinish = () => this.onAnimationFinish(true)
+    this.animation.oncancel = () => this.isExpanding = false
+  }
+
+  shrink() {
+    this.isClosing = true
+
+    const startHeight = `${ this.element.offsetHeight }px`
+    const endHeight = `${ this.summaryTarget.offsetHeight }px`
+    
+    if (this.animation) {
+      this.animation.cancel()
+    }
+
+    this.animation = this.animate(startHeight, endHeight)
+
+    this.animation.onfinish = () => this.onAnimationFinish(false)
+    this.animation.oncancel = () => this.isClosing = false
+  }
+
+  animate(startHeight, endHeight) {
+    return this.element.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 300,
+      easing: 'ease-out'
+    })
+  }
+
+  onAnimationFinish(open) {
+    this.element.open = open
+
+    this.resetState()
+    this.element.style.height = this.element.style.overflow = ''
+  }
+}
