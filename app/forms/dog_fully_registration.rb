6f02c06815a1c76df02e4b49725f31d95339ae20
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-04-04 22:27:38 +0900

    articleとfacebookの表示を修正

---
+++ app/forms/dog_fully_registration.rb
@@ -0,0 +1,29 @@
+class DogFullyRegistration
+  include ActiveModel::Model
+  attr_accessor :name, :castration, :public, :user_id,
+                :registration_number, :dogrun_place_id, :agreement
+
+  #validates
+  with_options presence: true do
+    validates :name, length: { maximum: 50 }
+    validates :registration_number
+    validates :user_id
+    validates :dogrun_place_id
+    validates :agreement, acceptance: true
+
+  end
+
+  def save
+    dog = Dog.create(
+      name: name,
+      castration: castration,
+      public: public,
+      user_id: user_id
+    )
+    RegistrationNumber.create!(
+      registration_number: registration_number,
+      dog_id: dog.id,
+      dogrun_place_id: dogrun_place_id
+    )
+  end
+end
