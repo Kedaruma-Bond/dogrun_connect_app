39b3181271a6cb4b88048e533ea818409acc28df
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-05-22 14:33:07 +0900

    top画面の部分更新機能trial

--- app/javascript/controllers/reloadable_controller.js
+++ app/javascript/controllers/reloadable_controller.js
@@ -1,11 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["reloadPart"]
+
   connect() {
     document.addEventListener("visibilitychange", () => {
       if (!document.hidden) {
-        location.reload();
+        // location.reload();
+        this.reloadPartTargets.forEach((reloadPart) => {
+          reloadPart.src = reloadPart.src
+        })
       }
-    });
+    })
   }
 }
