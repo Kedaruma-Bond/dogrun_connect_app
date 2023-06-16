b2cb7ce786d04822b0318e554be346a88fa97830
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-06-16 11:42:54 +0900

    #78 request spec admin/dogs_spec search action追加

--- spec/requests/admin/posts_spec.rb
+++ spec/requests/admin/posts_spec.rb
@@ -321,7 +321,7 @@ RSpec.describe Admin::PostsController, type: :request do
   end
 
   describe 'GET #search' do
-    context '管理者ユーザーでログインしているとき' do
+    describe '管理者ユーザーでログインしているとき' do
       before { admin_log_in_as(admin_1) }
 
       example '正常なレスポンスがかえり正しい投稿が表示されること' do
@@ -330,25 +330,25 @@ RSpec.describe Admin::PostsController, type: :request do
         expect(assigns(:posts)).to include(post_1)
       end
     end
-  end
 
-  describe '一般ユーザーでログインしているとき' do
-    before do
-      admin_log_in_as(general)
-      get search_admin_posts_path
+    describe '一般ユーザーでログインしているとき' do
+      before do
+        admin_log_in_as(general)
+        get search_admin_posts_path
+      end
+      
+      example 'エラーメッセージが表示されてhome画面にリダイレクトされること' do
+        expect(flash[:error]).to eq(I18n.t('defaults.not_authorized'))
+        expect(response).to redirect_to(root_path)
+      end
     end
-    
-    example 'エラーメッセージが表示されてhome画面にリダイレクトされること' do
-      expect(flash[:error]).to eq(I18n.t('defaults.not_authorized'))
-      expect(response).to redirect_to(root_path)
-    end
-  end
-
-  describe 'ログインしていないとき' do
-    example 'エラーメッセージが表示されて管理者ログイン画面にリダイレクトされること' do
-      get search_admin_posts_path
-      expect(flash[:error]).to eq(I18n.t('defaults.require_login'))
-      expect(response).to redirect_to(admin_login_path)
+  
+    describe 'ログインしていないとき' do
+      example 'エラーメッセージが表示されて管理者ログイン画面にリダイレクトされること' do
+        get search_admin_posts_path
+        expect(flash[:error]).to eq(I18n.t('defaults.require_login'))
+        expect(response).to redirect_to(admin_login_path)
+      end
     end
   end
 end
