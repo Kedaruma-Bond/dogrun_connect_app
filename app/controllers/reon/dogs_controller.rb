c950c26cf72c063efbdc7c5669518ffcf05180dd
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-06-28 12:51:06 +0900

    backボタンの無限ループ防止メソッド実装+disable_controller.js修正

--- app/controllers/reon/dogs_controller.rb
+++ app/controllers/reon/dogs_controller.rb
@@ -4,6 +4,8 @@ class Reon::DogsController < Reon::DogrunPlaceController
   before_action :correct_dog_owner, only: %i[edit update]
 
   def show
+    session[:previous_path] = request.referer unless previous_action_was_edit?
+    
     @user = User.find(@dog.user_id)
     @entries = Entry.where(dog: @dog).where(registration_number_id: @registration_number.id).joins(:registration_number).where(registration_number: { dogrun_place: @dogrun_place } ).sort.reverse
     @encount_dog = EncountDog.where(user_id: current_user.id).find_by(dog_id: @dog)
@@ -32,18 +34,22 @@ class Reon::DogsController < Reon::DogrunPlaceController
 
   private
 
-  def set_dog_and_registration_number
-    @dog = Dog.find(params[:id])
-    @registration_number = RegistrationNumber.where(dog_id: @dog.id).find_by(dogrun_place: @dogrun_place)
-  end
-  
-  def dog_params
-    params.require(:dog).permit(
-      :name, :birthday, :breed, :castration, :public,
-      :owner_comment, :sex, :weight, :filming_approval, :sns_post_approval,
-      :thumbnail, :mixed_vaccination_certificate, :rabies_vaccination_certificate, :license_plate,
-      :date_of_mixed_vaccination, :date_of_rabies_vaccination, :registration_prefecture_code,
-      :registration_municipality, :municipal_registration_number
-    ).merge(user_id: current_user.id)
-  end
+    def previous_action_was_edit?
+      request.referer&.include?("edit")
+    end
+
+    def set_dog_and_registration_number
+      @dog = Dog.find(params[:id])
+      @registration_number = RegistrationNumber.where(dog_id: @dog.id).find_by(dogrun_place: @dogrun_place)
+    end
+    
+    def dog_params
+      params.require(:dog).permit(
+        :name, :birthday, :breed, :castration, :public,
+        :owner_comment, :sex, :weight, :filming_approval, :sns_post_approval,
+        :thumbnail, :mixed_vaccination_certificate, :rabies_vaccination_certificate, :license_plate,
+        :date_of_mixed_vaccination, :date_of_rabies_vaccination, :registration_prefecture_code,
+        :registration_municipality, :municipal_registration_number
+      ).merge(user_id: current_user.id)
+    end
 end
--- app/javascript/controllers/disable_controller.js
+++ app/javascript/controllers/disable_controller.js
@@ -8,23 +8,17 @@ export default class extends Controller {
   connect() {
     this.element.dataset['action'] = 'submit->disable#disableForm'
     this.backButton = this.element.querySelector('[data-action="back-button#goBack"]')
+    this.submitButton = this.element.querySelector("input[type='submit']")
   }
+
 
   disableForm() {
-    const submitButtons = this.submitButtons()
-    const isBackButtonPressed = submitButtons.some(button => this.backButton === document.activeElement)
+    const isBackButtonPressed = this.backButton && this.backButton === document.activeElement
 
-    console.log(isBackButtonPressed)
-
-    submitButtons.forEach((button) => {
-      if (!isBackButtonPressed && button !== this.backButton) {
-        button.disabled = true
-        button.value = this.withValue
+      if(!isBackButtonPressed) {
+        this.submitButton.disabled = true
+        this.submitButton.value = this.withValue
       }
-    })
   }
 
-  submitButtons() {
-    return Array.from(this.element.querySelectorAll("input[type='submit']"))
-  }
 }
