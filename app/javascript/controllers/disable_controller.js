8da2c937e25217e1120b2dda89662e74c460b768
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-08-09 09:53:09 +0900

    backbuttonを変更

--- app/javascript/controllers/disable_controller.js
+++ app/javascript/controllers/disable_controller.js
@@ -8,19 +8,13 @@ export default class extends Controller {
 
   connect() {
     this.element.dataset['action'] = 'submit->disable#disableForm'
-    this.backButton = this.element.querySelector('[data-action="back-button#goBack"]')
   }
 
   disableForm() {
-    const isBackButtonPressed = this.backButton && this.backButton === document.activeElement
-
-    if(!isBackButtonPressed) {
-      this.submitButtonTargets.forEach((button) => {
-        button.disabled = true
-      })
-      this.buttonTextTarget.textContent = this.withValue
-      this.spinnerTarget.classList.remove("hidden")
-    }
+    this.submitButtonTargets.forEach((button) => {
+      button.disabled = true
+    })
+    this.buttonTextTarget.textContent = this.withValue
+    this.spinnerTarget.classList.remove("hidden")
   }
-
 }
