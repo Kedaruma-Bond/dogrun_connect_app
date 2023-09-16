dd6787fb19fa8ddd447c6e7adf90cdd6d46a2626
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-09-16 10:21:35 +0900

    monthly_dateグラフのdata収集修正

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -18,7 +18,7 @@ class Admin::DashboardsController < Admin::BaseController
       @entries_over_the_past_month = Entry.where(entry_at: from..to).joins(:registration_number).where(registration_number: { dogrun_place_id: current_user.dogrun_place_id })
       @registration_dogs = RegistrationNumber.where(dogrun_place: current_user.dogrun_place)
       @new_registration_dogs_over_the_past_month = RegistrationNumber.where(dogrun_place_id: current_user.dogrun_place_id).where(created_at: from..to)
-      entries = Entry.joins(:registration_number).where(registration_number: { dogrun_place_id: current_user.dogrun_place_id })
+      entries = Entry.includes(:registration_number).where(registration_number: { dogrun_place_id: current_user.dogrun_place_id })
       @posts_over_the_past_month = Post.where(dogrun_place: current_user.dogrun_place).where(created_at: from..to)
     end
 
@@ -26,16 +26,22 @@ class Admin::DashboardsController < Admin::BaseController
       e.entry_at.strftime('%Y%m%d')
     end
 
-    # @entries_daily = entries_daily.each_with_object(Hash.new(0)){|v,o| o[v]+=1}
     @entries_daily = (@two_weeks_ago..@today).map do |date|
       [date.strftime('%Y%m%d'), entries_daily.count(date.strftime('%Y%m%d'))]
     end.to_h
 
+    # entries_monthly = entries.group("DATE_TRUNC('month', entry_at)").count
     entries_monthly = entries.map do |e|
-      e.entry_at.strftime('%Y-%b')
+      e.entry_at.strftime('%Y-%m')
     end
-    
-    @entries_monthly = entries_monthly.each_with_object(Hash.new(0)){|v,o| o[v]+=1}
-  end
 
+    entries_monthly = entries_monthly.each_with_object(Hash.new(0)){|v,o| o[v]+=1}
+    all_months = (@one_year_ago..@today).to_a.map { |date| date.beginning_of_month }.uniq
+
+    @entries_monthly = all_months.map do |month|
+      month_key = month.strftime('%Y-%m')
+      [month_key, entries_monthly[month_key] || 0]
+    end.to_h
+
+  end
 end
