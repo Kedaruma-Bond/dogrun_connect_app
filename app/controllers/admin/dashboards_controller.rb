a326026f0c9b2468e70ee822d0df6c98ebb900f7
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-09-20 16:09:00 +0900

    admin ランキング機能実装

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,11 +1,12 @@
 class Admin::DashboardsController < Admin::BaseController
   before_action :check_grand_admin, only: %i[grand_admin_index]
-  before_action :set_time_var, only: %i[grand_admin_index index graph_section]
+  before_action :set_var, only: %i[grand_admin_index index graph_section entries_ranking]
 
   def index
     @entries_over_the_past_month = Entry.where(entry_at: @from..@to).joins(:registration_number).where(registration_number: { dogrun_place_id: current_user.dogrun_place_id })
     @registration_dogs = RegistrationNumber.where(dogrun_place: current_user.dogrun_place)
     @new_registration_dogs_over_the_past_month = RegistrationNumber.where(dogrun_place_id: current_user.dogrun_place_id).where(created_at: @from..@to)
+
   end
 
   def grand_admin_index
@@ -24,6 +25,43 @@ class Admin::DashboardsController < Admin::BaseController
     else
       @entries = @q.result.includes(:registration_number).where(registration_numbers: { dogrun_place_id: current_user.dogrun_place_id })
     end
+  end
+
+  def entries_ranking
+    entries_count = Entry
+      .includes(:registration_number)
+      .where(registration_numbers: { dogrun_place_id: current_user.dogrun_place_id })
+      .where(created_at: @one_year_ago.beginning_of_day..@today.end_of_day)
+    return if entries_count.size <= 300
+    
+    entries_ranking = entries_count
+      .group(:dog_id)
+      .order('count_id DESC')
+      .count.to_a
+
+    p "*" * 10
+    p entries_ranking
+    p "*" * 10
+    
+
+    @entries_ranking = []
+    i = 0
+    t = 0
+    while i < 5
+      @entries_ranking << [
+        i + 1,
+        Dog.find(entries_ranking[t][0]),
+        entries_ranking[t][1]
+      ]
+      t += 1
+      next if t >= 1 && entries_ranking[t - 1][1] == entries_ranking[t][1]
+      i += 1
+    end
+
+    p "*" * 10
+    p @entries_ranking
+    p "*" * 10
+
   end
 
   def graph_section
@@ -56,7 +94,7 @@ class Admin::DashboardsController < Admin::BaseController
 
 
   private 
-    def set_time_var
+    def set_var
       @to = Time.current.at_end_of_day
       @from = (@to - 1.month)
       @today = Date.today
