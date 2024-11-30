7358165a5d37401297fcd2437588b57490b5c814
Kedaruma-Bond <brianbondwhippet@gmail.com>
2024-12-01 02:56:01 +0900

    ssl_trueオプション削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,6 @@ Rails.application.configure do
   # config.cache_store = :redis_cache_store
   config.cache_store = :redis_cache_store, {
   url: ENV['REDIS_URL'],
-  ssl: true,
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
