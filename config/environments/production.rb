0a1d8e719e17405e3b6d57775a69aa91d0d21bb0
Kedaruma-Bond <brianbondwhippet@gmail.com>
2024-12-01 01:54:36 +0900

    ssl_trueを再々追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,7 @@ Rails.application.configure do
   # config.cache_store = :redis_cache_store
   config.cache_store = :redis_cache_store, {
   url: ENV['REDIS_URL'],
+  ssl: true,
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 $redis = Redis.new(
   url: ENV['REDIS_URL'],
+  ssl: true,
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
